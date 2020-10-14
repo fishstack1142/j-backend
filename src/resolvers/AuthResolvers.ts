afbe4e2136bc33c5e7e713a910a8d9e448a700ea
fishstack1142 <fishstack1142@gmail.com>
2020-10-14 21:15:49 +0700

    add mutation with typegoose

--- src/resolvers/AuthResolvers.ts
+++ src/resolvers/AuthResolvers.ts
@@ -1,10 +1,10 @@
-import { Resolver, Query } from 'type-graphql'
+import { Resolver, Query, Mutation, Arg } from 'type-graphql'
 import { User, UserModel } from '../entities/User'
- 
-@Resolver()
-export class AuthResolvers { 
 
-    @Query(() => [User], {nullable: 'items'}) //for nullable
+@Resolver()
+export class AuthResolvers {
+
+    @Query(() => [User], { nullable: 'items' }) //for nullable
     async users(): Promise<User[] | null> {
 
         try {
@@ -16,4 +16,33 @@ export class AuthResolvers {
     }
 
 
- }
+
+    @Mutation(() => User)
+    async createUser(
+        @Arg('username')
+        username: string,
+
+        @Arg('email')
+        email: string,
+
+        @Arg('password')
+        password: string
+    ) {
+
+        try {
+            const newUser = await UserModel.create({
+                username,
+                email,
+                password
+            })
+
+            await newUser.save()
+
+            return newUser
+        } catch (error) {
+            console.log(error)
+            throw error
+        }
+    }
+
+}
